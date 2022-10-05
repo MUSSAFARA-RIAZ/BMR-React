a7627ed61325a3749b3869fdc8ee6bc7b04301da
Mussafara Riaz <mussafarariaz@gmail.com>
2022-10-05 19:23:49 +0500

    commit

---
+++ src/Calculator.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+export default function Calculator() {
+  return (
+    <div className='parent-div'>
+     <h1>BMR and Daily Calorie Calculator</h1>
+     <form action="">
+      <label>Gender</label>
+     
+      
+     </form>
+
+      
+    </div>
+  )
+}
