86bbc9c907a055e6ae156e0585f7262ec76e8a3b
Mussafara Riaz <mussafarariaz@gmail.com>
2022-10-06 22:52:28 +0500

    commit

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -2,11 +2,36 @@ import React from 'react';
 import './index.css';
 
 export default function Calculator() {
+ const genderoption=["male","female"];
+ 
+
   return (
     <div className='parent-div'>
      <h1>BMR and Daily Calorie Calculator</h1>
      <form action="">
       <label>Gender</label>
+     <input type="radio" name="gender" id="male"/>Male
+        
+   <input type="radio" name="gender" id="Female"/>Female
+   <label htmlFor="weight">Weight</label>
+   <input type="text" placeholder='Enter your weight' />
+   <label htmlFor="height">Height</label>
+   <input type="text" placeholder='Enter Your height' />
+   
+    
+    
+
+       
+      
+
+
+
+      
+      
+      
+      
+      
+
      
       
      </form>
